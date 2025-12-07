88f334758d3cf643ff17f68718248442635669e1
mgkl92 <zst_lff@126.com>
2025-12-07 13:28:29 +0000

    Add support for dynamic add/remove worker threads

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <chrono>
 #include <condition_variable>
 #include <functional>
 #include <future>
@@ -21,7 +22,8 @@ using result_of_t = typename std::result_of<Func(Args...)>::type;
 
 class ThreadPool {
  public:
-  explicit ThreadPool(uint nthreads);
+  explicit ThreadPool(uint threads_cnt,
+                      std::chrono::milliseconds = DEFAULT_IDLE_TIME);
 
   ~ThreadPool();
 
@@ -29,9 +31,15 @@ class ThreadPool {
 
   ThreadPool(ThreadPool&&) = delete;
 
+  void resize(uint new_threads_cnt);
+
+  uint get_thread_count() const;
+
   template <typename Func, typename... Args>
   auto submit(Func&& f, Args&&... args)
       -> std::future<result_of_t<Func, Args...>>;
+
+  static constexpr std::chrono::milliseconds DEFAULT_IDLE_TIME{30 * 1000};
 
  private:
   void worker();
@@ -41,5 +49,9 @@ class ThreadPool {
   std::condition_variable m_cond_;
   bool m_running_;
   std::atomic<uint> m_ready_cnt_;
+  uint m_current_threads_cnt_;
+  uint m_min_threads_cnt_;
+  std::chrono::milliseconds m_idle_timeout;
+  uint m_threads_cnt_to_remove_;
 };
 }  // namespace mgkl
--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -3,13 +3,21 @@
 #include "thread_pool.h"
 
 namespace mgkl {
-ThreadPool::ThreadPool(uint nthreads) : m_running_{true}, m_ready_cnt_{0} {
+constexpr std::chrono::milliseconds ThreadPool::DEFAULT_IDLE_TIME;
+
+ThreadPool::ThreadPool(uint nthreads, std::chrono::milliseconds idle_timeout)
+    : m_running_{true},
+      m_ready_cnt_{0},
+      m_idle_timeout{idle_timeout},
+      m_threads_cnt_to_remove_{0} {
   assert(nthreads > 0 && "invalid parameter!");
   nthreads = std::min(nthreads, std::thread::hardware_concurrency());
 
   for (uint i = 0; i < nthreads; ++i) {
     m_threads_.emplace_back(std::bind(&ThreadPool::worker, this));
   }
+  m_current_threads_cnt_ = static_cast<uint>(m_threads_.size());
+  m_min_threads_cnt_ = m_current_threads_cnt_;
 
   // waiting for all worker threads to be ready
   while (m_ready_cnt_.load() < nthreads) {
@@ -17,18 +25,58 @@ ThreadPool::ThreadPool(uint nthreads) : m_running_{true}, m_ready_cnt_{0} {
   }
 }
 
+inline void mgkl::ThreadPool::resize(uint new_threads_cnt) {
+  std::lock_guard<std::mutex> lock(m_mtx_);
+
+  if (!m_running_ || (new_threads_cnt == m_current_threads_cnt_)) {
+    return;
+  }
+
+  assert(new_threads_cnt > 0 && "Invalid new thread count");
+  new_threads_cnt =
+      std::min(new_threads_cnt, std::thread::hardware_concurrency());
+  if (new_threads_cnt > m_current_threads_cnt_) {
+    uint threads_cnt_to_add = new_threads_cnt - m_current_threads_cnt_;
+    for (uint i = 0; i < threads_cnt_to_add; ++i) {
+      m_threads_.emplace_back(std::bind(&ThreadPool::worker, this));
+    }
+
+    m_current_threads_cnt_ = m_threads_.size();
+    m_min_threads_cnt_ = m_current_threads_cnt_;
+  } else {
+    m_min_threads_cnt_ = new_threads_cnt;
+    m_cond_.notify_all();
+  }
+
+  m_current_threads_cnt_ = new_threads_cnt;
+}
+
+inline uint mgkl::ThreadPool::get_thread_count() const {
+  return m_current_threads_cnt_;
+}
+
 void ThreadPool::worker() {
   m_ready_cnt_++;
 
   while (true) {
     std::function<void()> task;
-
+    bool is_timeout = false;
     {
       std::unique_lock<std::mutex> lock(m_mtx_);
-      m_cond_.wait(lock, [this]() { return !m_tasks_.empty() || !m_running_; });
+      m_cond_.wait_for(lock, m_idle_timeout,
+                       [this]() { return !m_tasks_.empty() || !m_running_; });
 
       if (!m_running_ && m_tasks_.empty()) {
         return;
+      }
+
+      if (is_timeout && m_current_threads_cnt_ > m_min_threads_cnt_) {
+        --m_current_threads_cnt_;
+        return;
+      }
+
+      if (m_tasks_.empty()) {
+        continue;
       }
 
       task = std::move(m_tasks_.front());
